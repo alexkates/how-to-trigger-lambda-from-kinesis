8292ef105c73d74ed1e263f6bb134cd8f6b43ab2
Daniel Alexander Kates <alex.kates@creditly.com>
2021-07-14 14:17:45 -0400

    initial commit

--- lib/how-to-trigger-lambda-from-kinesis-stack.ts
+++ lib/how-to-trigger-lambda-from-kinesis-stack.ts
@@ -1,9 +1,27 @@
 import * as cdk from '@aws-cdk/core';
+import * as kinesis from '@aws-cdk/aws-kinesis';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as lambdaEventSources from '@aws-cdk/aws-lambda-event-sources';
 
 export class HowToTriggerLambdaFromKinesisStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const stream = new kinesis.Stream(this, 'MyKinesisStream', {
+      streamName: 'MyKinesisStream',
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'Function', {
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      functionName: 'KinesisMessageHandler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+
+    const eventSource = new lambdaEventSources.KinesisEventSource(stream, {
+      startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+    });
+
+    lambdaFunction.addEventSource(eventSource);
   }
 }
